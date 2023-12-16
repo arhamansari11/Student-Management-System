3c3af053aa7fbcb93446f843549b8a32a9145ab0
Arham <arhamansari454647@gmail.com>
2023-12-16 15:49:30 +0500

    Students-MS

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Routing from "./routing/Routing";
+import './App.css'
 
 function App() {
   return (
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,45 +1,47 @@
-// Navbar.js
 import React from "react";
 import { Link } from "react-router-dom";
 import { Dashboard, People, Book, Assignment } from "@mui/icons-material";
-import "../styles/navbar.css";
+import "../styles/navbar.css"; // Import the CSS file
 
 const Navbar = () => {
   return (
     <>
       <div className="container-fluid">
         <div className="row">
-          <div className="col-6">
+          <div className="col-lg-2 col-md-3 col-sm-4 col-5">
             <div
-              className="row"
+              className="row bg-clr"
               style={{ display: "flex", flexDirection: "column" }}
             >
-              <div className="col-7">
-                <b>Muhammad Arham</b>
+              <div className="col-11 icons">
+                <b style={{fontSize : "19px"}}>Muhammad Arham</b>
               </div>
-              <div className="col-7">
-                <Link to="/" style={{ textDecoration: "none" }}>
+              <div className="col-7 icons">
+                <Link to="/" className="nav-link">
                   <Dashboard />
                   <span>Dashboard</span>
                 </Link>
               </div>
-              <div className="col-7">
-                <Link to="/students" style={{ textDecoration: "none" }}>
+              <div className="col-7 icons">
+                <Link to="/students" className="nav-link">
                   <People />
                   <span>Students</span>
                 </Link>
               </div>
-              <div className="col-7">
-                <Link to="/courses" style={{ textDecoration: "none" }}>
+              <div className="col-7 icons">
+                <Link to="/courses" className="nav-link">
                   <Book />
                   <span>Courses</span>
                 </Link>
               </div>
-              <div className="col-7">
-                <Link to="/attendance" style={{ textDecoration: "none" }}>
+              <div className="col-7 icons">
+                <Link to="/attendance" className="nav-link">
                   <Assignment />
                   <span>Attendance</span>
                 </Link>
+              </div>
+              <div className="col-7 icons">
+
               </div>
             </div>
           </div>
